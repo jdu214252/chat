7e78d824e07e5380b67a09aa4b67e624515671cc
jdu214252 <214252x@jdu.uz>
2024-11-29 14:18:21 +0500

    Initial commit

--- App.js
+++ App.js
@@ -1,46 +1,25 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { theme } from './theme'
+import { ThemeProvider } from './ThemeContext'
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+import AppContainer from './AppContainer';
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
-  );
+    <ThemeProvider>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" backgroundColor={theme.colors.primary}></StatusBar>
+      <AppContainer />
+    </SafeAreaView>
+    </ThemeProvider>
+  )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: theme.colors.white
+  }
+})
